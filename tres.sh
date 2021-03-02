549f684e183ed35c9e737060a8275f58482e0644
SohIsa <guerra-isa@hotmail.com>
2021-03-02 12:57:41 -0300

    terceiro e quarto

--- tres.sh
+++ tres.sh
@@ -5,3 +5,5 @@ mkdir /tmp/"$(date +'%Y-%m-%d')"
 cp -r /home/isa/BashAtiv /tmp/"$(date +'%Y-%m-%d')"
 
 tar cvzf "$(date +'%Y-%m-%d')".tar.gz /tmp/"$(date +'%Y-%m-%d')"
+
+rm -r /tmp/"$(date +'%Y-%m-%d')"
