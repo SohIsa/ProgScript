c7d1f12bd263611f7be015cf9cf8688018f61ae1
SohIsa <guerra-isa@hotmail.com>
2021-04-20 18:19:15 -0300

    eu sei mas...

--- while.case/help_test2.sh
+++ while.case/help_test2.sh
@@ -40,6 +40,32 @@ while [ "${busca}" != "SAIR" ]; do
 		[[ a > b ]] || echo 'a não vem depois que b'
 		[[ az < za ]] && echo 'az vem antes que za'
 		[[ a = a ]] && echo ' a é o mesmo que a'"; fi
+	if [ $1 = arquivos ]; then
+	echo "ARQUIVOS:
+	Opções para certos tipos de arquivos. Todos os casos retornam true (0) se o arquivo existir.
+ 	 -b file      True if file is a Block special device. [[ -b demofile ]]
+	 -c file      True if file is a Character special device. [[ -c demofile ]]
+	 -d file      True if file is a Directory. [[ -d demofile ]]
+	 -e file      True if file Exists. [[ -e demofile ]]
+	 -f file      True if file is a regular File. [[ -f demofile ]]
+	 -g file      True if file has its set-group-id bit set. [[ -g demofile ]]
+	 -G file      True if file is owned by the current effective group id. [[ -G demofile ]]
+	 -k file      True if file has its -sticky- bit set. [[ -k demofile ]]
+	 -h file      True if file is a symbolic Link. [[ -h demofile ]]
+	 -L file      True if file is a symbolic Link. [[ -L demofile ]]
+	 -O file      True if file is owned by the current effective user id. [[ -O demofile ]]
+	 -p file      True if file is a named Pipe. [[ -p demofile ]]
+	 -r file      True if file is readable. [[ -r demofile ]]
+	 -S file      True if file is a Socket. [[ -S demofile ]]
+	 -s file      True if file has a Size greater than zero. [[ -s demofile ]]
+	 -t [FD]      True if FD is opened on a terminal.  If FD is omitted, it defaults to 1 (standard output). [[ -t demofile ]]
+	 -u file      True if file has its set-user-id bit set. [[ -L demofile ]]
+	 -w file      True if file is writable. [[ -w demofile ]]
+	 -x file      True if file is executable. [[ -x demofile ]]
+	file1 -ef file2    True if file1 and file2 have the same device and inode numbers,  i.e. they are hard links to each other.
+	File Age Tests (modification date)
+	file1 -nt file2     True if file1 is newer than file2. [[ demofile1 -nt $DEMO ]]
+	file1 -ot file2     True if file1 is older than file2. [[ $DEMO -ot demofile2 ]]"; fi
 	
 
         read -p " 
